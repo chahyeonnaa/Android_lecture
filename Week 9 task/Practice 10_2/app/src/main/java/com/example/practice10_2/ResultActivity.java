6da7a546051c2549d11c740f9cb14d55155bc4c9
chahyeonnaa <gusdk21650@naver.com>
2022-05-04 15:17:23 +0900

    :tulip: 실습 10-2, 직접 풀어보기 10-2

--- Week 9 task/Practice 10_2/app/src/main/java/com/example/practice10_2/ResultActivity.java
+++ Week 9 task/Practice 10_2/app/src/main/java/com/example/practice10_2/ResultActivity.java
@@ -12,18 +12,30 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 
 public class ResultActivity extends AppCompatActivity{
-    Integer tvID[] = {R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5, R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9};
-    Integer rBarID[] = {R.id.rBar1, R.id.rBar2, R.id.rBar3, R.id.rBar4, R.id.rBar5, R.id.rBar6, R.id.rBar7, R.id.rBar8, R.id.rBar9};
-    Integer imgFiled[] = {R.id.img1, R.id.img2, R.id.img3, R.id.img4, R.id.img5, R.id.img6, R.id.img7, R.id.img8, R.id.img9};
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); setContentView(R.layout.activity_result);
         setTitle("투표 결과");
+        Integer tvID[] = {R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5, R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9};
+        Integer rBarID[] = {R.id.rBar1, R.id.rBar2, R.id.rBar3, R.id.rBar4, R.id.rBar5, R.id.rBar6, R.id.rBar7, R.id.rBar8, R.id.rBar9};
+        Integer imgFiled[] = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5, R.drawable.pic6, R.drawable.pic7, R.drawable.pic8, R.drawable.pic9};
+
         Intent intent = getIntent();
         int[] voteResult = intent.getIntArrayExtra("VoteCount");
         String[] imageName = intent.getStringArrayExtra("ImageName");
+
+        TextView tvTop = findViewById(R.id.tvTop);
+        ImageView ivTop = findViewById(R.id.ivTop);
+        int maxEntry = 0;
+
+        for (int i = 1; i < voteResult.length; i++)
+        {
+            if (voteResult[maxEntry] < voteResult[i])
+                maxEntry = i;
+        }
+        tvTop.setText(imageName[maxEntry]);
+        ivTop.setImageResource(imgFiled[maxEntry]);
+
         TextView tv[] = new TextView[imageName.length];
         RatingBar rBar[] = new RatingBar[imageName.length];
 
